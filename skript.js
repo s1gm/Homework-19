cdeb7e1e5deb51634573ac4bd725e855c0fcdcd1
Anastasia Litvinchuk <lytvynchukmark@gmail.com>
2023-07-24 18:34:29 +0300

    Homework-19

---
+++ skript.js
@@ -0,0 +1,29 @@
+let result = 5+5+'5';
+
+console.log(result);
+
+
+
+let email =  "lytvynchukmark@gmail.com";
+
+console.log(email.includes("@"))
+
+
+
+let str = "My";
+
+let str2 = "name";
+
+let str3 = "is";
+
+let fullName = str + str2 + str3;
+
+console.log(fullName + "Viktor");
+
+
+
+let userName = "Олександро";
+
+let payment = 300
+
+let total = alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`)
